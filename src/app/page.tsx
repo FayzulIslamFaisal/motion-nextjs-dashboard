248ad5509767a70766f3be3d416b69e73c9f60f3
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-09-22 18:40:48 +0600

    install shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <section className="container mx-auto px-4">
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <Button variant="destructive">Click Me</Button>
     </div>
+    </section>
   );
 }
